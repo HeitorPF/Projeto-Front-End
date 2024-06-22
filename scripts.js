446f4955d2508eaec36f4fe601f5d8909c1b2e4f
HeitorPF <heitorpf12.hf@gmail.com>
2024-06-21 21:11:41 -0300

    pesquisar parcial

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let lista = document.getElementById("li");
+
+const pesqbot = document.getElementById("botao-pesquisar")
 const form = document.getElementById('formulario');
 const userList = document.getElementById('userList');
 const addButton = document.getElementById('botao-adicionar');
@@ -46,6 +49,29 @@ function deleteAllUsers() {
     renderUserList();
 }
 
+function pesquisa(){
+    const dados = JSON.parse(localStorage.getItem('user'))
+    let pesq = search.value + '';
+    for(let i=0; i<cadastrados.length;i++){
+        let nome = dados[i].name + '';
+        let email = dados[i].email + '';
+
+        if(nome.includes(pesq) || email.includes(pesq)){
+            limparForm();
+            const listItem = document.createElement('li');
+            listItem.textContent = `${dados[i].timestamp} - Nome: ${dados[i].name}, E-mail: ${dados[i].email}`;
+            userList.appendChild(listItem);
+        }
+        
+    }
+}
+
+function remover(mail){
+    localStorage.removeItem(mail);
+}
+
+
+pesqbot.addEventListener('click', pesquisa);
 renderUserList();
 addButton.addEventListener('click', addUser);
 clearButton.addEventListener('click', limparForm);
